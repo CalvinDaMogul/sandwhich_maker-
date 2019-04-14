f252a8a9534e7d21943e2cc1e77ae84b5d8bb92f
Calvin J Foster <cal.j.foster@gmail.com>
2019-04-14 18:28:30 -0500

    finish code

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,11 @@
+import store from './components/store.js';
+import buttonEvents from './helpers/buttonEvents.js';
+
 console.log('hi');
 
 const init = () => {
+   // store.makeSub();
+    buttonEvents.submitButton();
     
 };
 
